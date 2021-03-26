715114b016c0575c505509da5c760020d1359812
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-03-26 12:22:57 +0800

    Changed brand name.

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,9 @@ const Footer = () => {
 		<footer>
 			<Container>
 				<Row>
-					<Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+					<Col className='text-center py-3'>
+						Copyright &copy; BEAMAZEDD SHOP
+					</Col>
 				</Row>
 			</Container>
 		</footer>
