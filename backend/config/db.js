b1504710aa036dbe1db17f01dc0b1dbf6b8d0015
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-03-26 13:09:15 +0800

    Connected project to mongodb.

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import colors from 'colors';
+
+const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+			useCreateIndex: true,
+		});
+
+		console.log(`MongoDB Connected: ${conn.connection.host}`.bgWhite.green);
+	} catch (err) {
+		console.log(`Error: ${err.message}`);
+		process.exit();
+	}
+};
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 import colors from 'colors';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
