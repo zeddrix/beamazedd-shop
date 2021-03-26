3c5ac60293f7d1b7f0a94eb42003db4857ac0530
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-03-26 12:12:58 +0800

    Used ES6 syntax for imp and exp.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const products = require('./data/products');
-const colors = require('colors');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
+import colors from 'colors';
 
 dotenv.config();
 
