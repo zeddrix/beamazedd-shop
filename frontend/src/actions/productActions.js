e66aae3ed7f3350ca74e21cb60c29ccd82fc92cd
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-03-27 11:57:37 +0800

    Created productList reducer and action.

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,25 @@
+import * as c from './constants';
+import axios from 'axios';
+
+export const listProducts = () => async (dispatch) => {
+	try {
+		dispatch({
+			type: c.PRODUCT_LIST_REQUEST,
+		});
+
+		const { data } = await axios.get('/api/products');
+
+		dispatch({
+			type: c.PRODUCT_LIST_REQUEST,
+			payload: data,
+		});
+	} catch (err) {
+		dispatch({
+			type: c.PRODUCT_LIST_FAIL,
+			payload:
+				err.response && err.response.data.message
+					? err.response.data.message
+					: err.message,
+		});
+	}
+};
