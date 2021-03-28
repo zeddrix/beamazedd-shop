461e8c0302adaabc4f0f3a7eb3e4eef66e69f913
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-03-28 18:54:58 +0800

    Constants, reducer, & action for product details.

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -23,3 +23,26 @@ export const listProducts = () => async (dispatch) => {
 		});
 	}
 };
+
+export const listProductDetails = (id) => async (dispatch) => {
+	try {
+		dispatch({
+			type: c.PRODUCT_DETAILS_REQUEST,
+		});
+
+		const { data } = await axios.get(`/api/products/${id}`);
+
+		dispatch({
+			type: c.PRODUCT_DETAILS_SUCCESS,
+			payload: data,
+		});
+	} catch (error) {
+		dispatch({
+			type: c.PRODUCT_DETAILS_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -21,3 +21,28 @@ export const productListReducer = (state = { products: [] }, action) => {
 			return state;
 	}
 };
+
+export const productDetailsReducer = (
+	state = { product: { reviews: [] } },
+	action
+) => {
+	switch (action.type) {
+		case c.PRODUCT_DETAILS_REQUEST:
+			return {
+				loading: true,
+				...state,
+			};
+		case c.PRODUCT_DETAILS_SUCCESS:
+			return {
+				loading: false,
+				product: action.payload,
+			};
+		case c.PRODUCT_DETAILS_FAIL:
+			return {
+				loading: false,
+				error: action.payload,
+			};
+		default:
+			return state;
+	}
+};
