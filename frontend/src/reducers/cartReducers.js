6121a3d1a6f02baadf9399d17609fd7f10061aab
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-03-28 20:13:10 +0800

    Items in cart persists in LS.

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -16,7 +16,7 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
 			} else {
 				return {
 					...state,
-					cartItems: { ...state.cartItems, item },
+					cartItems: [...state.cartItems, item],
 				};
 			}
 		default:
