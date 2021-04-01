81ca8d654dbc3fb0a9fd853afbefc4efb5f43da7
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-04-01 14:38:22 +0800

    User auth endpoint.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const UserSchema = mongoose.Schema(
 	{
@@ -25,6 +26,10 @@ const UserSchema = mongoose.Schema(
 		timestamps: true,
 	}
 );
+
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+	return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', UserSchema);
 
