9222d9c0b188e1fbd28ffd91d17d77c169dba704
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-04-03 19:15:40 +0800

    Update user functionality.

--- frontend/src/reducer.js
+++ frontend/src/reducer.js
@@ -12,6 +12,7 @@ import {
 	userLoginReducer,
 	userRegisterReducer,
 	userUpdateProfileReducer,
+	userUpdateReducer,
 } from './reducers/userReducers';
 import {
 	getMyOrderReducer,
@@ -30,6 +31,7 @@ const reducer = combineReducers({
 	userUpdateProfile: userUpdateProfileReducer,
 	userList: userListReducer,
 	userDelete: userDeleteReducer,
+	userUpdate: userUpdateReducer,
 	orderCreate: orderCreateReducer,
 	orderDetails: orderDetailsReducer,
 	orderPay: orderPayReducer,
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -133,3 +133,26 @@ export const userDeleteReducer = (state = {}, action) => {
 			return state;
 	}
 };
+
+export const userUpdateReducer = (state = { user: {} }, action) => {
+	switch (action.type) {
+		case c.USER_UPDATE_REQUEST:
+			return {
+				loading: true,
+			};
+		case c.USER_UPDATE_SUCCESS:
+			return {
+				loading: false,
+				success: true,
+			};
+		case c.USER_UPDATE_FAIL:
+			return {
+				loading: false,
+				error: action.payload,
+			};
+		case c.USER_UPDATE_RESET:
+			return { user: {} };
+		default:
+			return state;
+	}
+};
