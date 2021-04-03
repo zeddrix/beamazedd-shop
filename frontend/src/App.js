a606d301e7022dff3e6ea3381b4370eab726a31e
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-04-03 18:53:06 +0800

    UserEditScreen UI.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
 
 const App = () => {
 	return (
@@ -30,6 +31,7 @@ const App = () => {
 					<Route exact path='/product/:id' component={ProductScreen} />
 					<Route exact path='/cart/:id?' component={CartScreen} />
 					<Route exact path='/admin/userlist' component={UserListScreen} />
+					<Route exact path='/admin/user/:id/edit' component={UserEditScreen} />
 					<Route exact path='/' component={HomeScreen} />
 				</Container>
 			</main>
---
+++ frontend/src/screens/UserEditScreen.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import FormContainer from '../components/FormContainer';
+import { getUserDetails } from '../actions/userActions';
+
+const UserEditScreen = ({ match, history }) => {
+	const userId = match.params.id;
+
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [isAdmin, setIsAdmin] = useState(false);
+
+	const dispatch = useDispatch();
+
+	const useDetails = useSelector((state) => state.userDetails);
+	const { loading, error, user } = useDetails;
+
+	useEffect(() => {}, []);
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<>
+			<Link to='/admin/userlist' className='btn btn-light my-3'>
+				Go Back
+			</Link>
+			<FormContainer>
+				<h1>Edit User</h1>
+
+				{loading ? (
+					<Loader />
+				) : error ? (
+					<Message variant='danger'>{error}</Message>
+				) : (
+					<Form onSubmit={submitHandler}>
+						<Form.Group>
+							<Form.Label>Name</Form.Label>
+							<Form.Control
+								type='name'
+								placeholder='Enter name'
+								value={name}
+								onChange={(e) => setName(e.target.value)}></Form.Control>
+						</Form.Group>
+
+						<Form.Group>
+							<Form.Label>Email Address</Form.Label>
+							<Form.Control
+								type='email'
+								placeholder='Enter email'
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}></Form.Control>
+						</Form.Group>
+
+						<Form.Group controlId='isadmin'>
+							<Form.Check
+								type='checkbox'
+								label='Is Admin'
+								checked={isAdmin}
+								onChange={(e) => setIsAdmin(e.target.checked)}></Form.Check>
+						</Form.Group>
+
+						<Button type='submit' variant='primary'>
+							Update
+						</Button>
+					</Form>
+				)}
+			</FormContainer>
+		</>
+	);
+};
+
+export default UserEditScreen;
