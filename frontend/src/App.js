da92a82f0d3c1896556ca7e3d7b6814750694b3f
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-04-05 10:08:39 +0800

    Admin ProductList UI.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
+import ProductListScreen from './screens/ProductListScreen';
 
 const App = () => {
 	return (
@@ -32,6 +33,11 @@ const App = () => {
 					<Route exact path='/cart/:id?' component={CartScreen} />
 					<Route exact path='/admin/userlist' component={UserListScreen} />
 					<Route exact path='/admin/user/:id/edit' component={UserEditScreen} />
+					<Route
+						exact
+						path='/admin/productlist'
+						component={ProductListScreen}
+					/>
 					<Route exact path='/' component={HomeScreen} />
 				</Container>
 			</main>
