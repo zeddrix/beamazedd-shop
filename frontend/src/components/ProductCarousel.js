11462cfdae2a4dec167de0ca05d415e11d946a27
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-04-05 20:45:43 +0800

    Fixed bugs.

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useDispatch, useSelector } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Carousel, Image } from 'react-bootstrap';
 import Loader from './Loader';
