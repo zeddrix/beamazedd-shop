0dbdd4650d79724183196d7538f92597afea8db6
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-04-05 12:49:58 +0800

    EditProductScreen UI.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 const App = () => {
 	return (
@@ -37,6 +38,11 @@ const App = () => {
 						exact
 						path='/admin/productlist'
 						component={ProductListScreen}
+					/>
+					<Route
+						exact
+						path='/admin/product/:id/edit'
+						component={ProductEditScreen}
 					/>
 					<Route exact path='/' component={HomeScreen} />
 				</Container>
